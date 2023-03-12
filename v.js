7c9580c6d657340716a1ac38e95c9c34ffc63ba5
Zyadhdhdh37 <115433642+Zyadhdhdh37@users.noreply.github.com>
2023-03-13 00:12:58 +0300

    Add files via upload

---
+++ v.js
@@ -0,0 +1,832 @@
+// // // the alrt dot goode
+ // console.log(+"22");
+// // console.log(1,6);
+// // Number(1)
+
+// // console.log((200).toString());
+
+// // // داله نقول لها كم ترجع ارقام
+
+// // console.log(1000.555.toFixed());
+// // // تحول النص الئ ارقام
+// // console.log(parseInt(1000));
+
+// // // داله تفحص ادا كانت الرقم رقم او لا
+// // console.log(Number.isInteger(600));
+// // // التقريب ال ابعد رقم ادا كان فوق النص والئ اقل قيمه ادا كان تحت النص 
+// // console.log(Math.round(99.44));
+// // // تقرب الئ اعلئ رقم 
+// // console.log(Math.ceil(99.44));
+// // // تقرب  الئ اقرب رقم 
+// // console.log(Math.floor(99.44));
+// // //  داله الاس 
+// //  console.log(Math.pow(4,6));
+// // // 
+// //  console.log(Math.trunc(5.55));
+
+// // let osma  = 55;
+
+// // // جمله اف القصيره 
+
+// //   osma=== 77 ?  console.log("kasm") : console.log(" soso"); 
+ 
+
+// // let arrys = [1, 'kams ','sosoe', 'arpor',['ahmed','ksomey']];
+// // console.log(arrys[0]= 'ahmed');
+
+// // console.log("dirction and f");
+
+
+// // let a=0 ;
+
+
+// // switch (a) {
+// //   case 0: console.log("ANS ABO LOHOM");
+    
+// //     break;
+
+// //   default:
+// //     console.log("HALO");
+// //     break;
+// // }
+// // //عايتم معنا خزن العنصر 33 في الموفع 13ويخلي الي قبله فاضيات
+// // let  myfrined =[1,3,5,5,67,7,8,8,8,"AHMED YADDN","ZKARY YASSIN", "SOSOE AHMED SAYED"];
+// // myfrined[13] = 33;
+// // //هاده الداله تقوم بارالاضافه الئ بدايه المصفوفه 
+// // myfrined.unshift("ahmed", 'kasm');
+// // console.log(myfrined);
+
+// // //تضيف من نهايه المصفوفه
+// // myfrined.push("yhmed", 'kasm');
+// // //تقوم بحدف اول عنصر داخل المصفوفه 
+// // myfrined.shift();
+// // //داله بحث ارسله العنصر  وهي ترجع موقعه 
+// //  let zezo = myfrined.indexOf(8 ,2);
+// // console.log(zezo);
+// //  //   مش من الاخير حرفيا داله تبحث من الاخير
+// // let zez = myfrined.lastIndexOf(8);
+// // console.log(zez);
+// // //ترتب  حسب الابجديه 
+// // console.log( myfrined.sort());
+// // //دال تعكس العناصر الاول تخليه الاهير والاخير الاول 
+// // console.log(myfrined.reverse())
+// // console.log(myfrined.sort().reverse())
+// // //    and the fuincon  end and stsrtتنسخ الداله كلها 
+// //  console.log(myfrined.slice())
+// //  console.log(myfrined.slice(2,3))
+
+// //  //ترجع مصفوفه نصيه 
+// //  console.log(myfrined.join());
+// //  //عايفصل بين النصوص بفراغات  اي شي نرسل له عايفصل  علئ حسبه 
+// //  console.log(myfrined.join(" "));
+// //  console.log(myfrined.join("|").toUpperCase())
+// //  let w= 1;
+// //  for (const wd in 10 ){
+// // console.log(15);
+
+// //  }
+// //  for (let i = 0; i<10; i++) {
+// // console.log(i);
+
+// //  }
+// //  //داله تكرر النص اللدي نرسله 
+// //  console.log("#".repeat(44));
+
+// //  //حلقه تكرار تطبع النصوص ونهمل الارقام 
+// // for (let i =0 ;i< myfrined.length ; i++)
+// // {
+// //   if (typeof myfrined[i] ==="number"){
+// //     continue;
+// //   }
+
+// //  console.log(myfrined[i]);
+
+// // }
+// // document.write('<div> ahmed yaddin and alhtar <\div>')
+
+
+
+
+// // document.write('<h1> AHAD YASSIN<\h1>');
+
+
+
+// // function brint( yarsstart,yarend){
+
+// // for( let i= yarsstart ; i <yarend; i++){
+
+// // console.log(i);
+
+// // }
+
+// // }
+
+// //   brint(1833, 1844);
+// // function sysrt(ahmed){
+
+// //   return ahmed;
+// // }
+// // console.log( sysrt(66));
+
+
+// // function numbers( num1, num2){
+// //  for(let i = num1 ;i<=num2 ;i++){;
+// // console.log(i);
+// //   if(i===15 || i ===19){
+
+    
+// // //the intrupttingيعمي عايوقف لما يتحقق الشر 
+
+// //     return `interuptting`;
+// //   }
+// //  }
+// // }
+// // console.log( numbers(10, 20));
+// // document.write(`<div>ahemd yassin </div>`);
+// // let s=[];
+// // function arysSS(...zezoarry){
+// //   {
+// // console.log(arrys.length)
+  
+// // }
+
+// // for( let i =0 ; i<zezoarry.length; i++){
+// //   console.log(zezoarry[i]);
+// // }  
+// // }
+
+
+// // arysSS(1,5,6,7,8,6655,4)
+
+
+// // function aomry(age , yars , cyoty){
+
+// // document.write(`<div>`);
+// // document.write(`<h3> the age ${age}<h3>`);
+
+// // if(yars ==2020){
+// //   document.write(`<h3> the yars ${yars}<h3>`);
+
+// // }
+// // document.write(`<h3> the cyoty ${cyoty}<h3>`);
+// // document.write(`</div>`);
+
+
+// // }
+
+// // aomry( 10, 2020, 'abe ')
+// // // ذ1TEMBET ALZERO تكليف الريبر 
+// // function showketails(name, age, stat ){
+      
+// //  let arrry=[];
+// //  arrry[0]=name;
+// //  arrry[1]=age;
+// //  arrry[2]=stat;
+// //  if(name == arrry[0] && age ==arrry[1] && stat == arrry[2] ){
+
+// //   document.write( `  :  haalo ${name}`);
+// //   document.write( `  :  your is age ${age}`);
+// //   document.write( `  :  you good ${stat}`);
+
+// //  }
+
+// // if (name == arrry[1] && age ==arrry[0] && stat == arrry[2] ){
+// //   document.write( `  :  haalo ${name}`);
+// // document.write( `  :  your is age ${age}`);
+// // document.write( `  :  you good ${stat}`);
+
+// // }
+// // if (name == arrry[1] && age ==arrry[2] && stat == arrry[0] ){
+// // document.write( `  :  haalo ${name}`);
+// // document.write( `  :  your is age ${age}`);
+// // document.write( `  :  you good ${stat}`);
+
+// // }
+
+// // }
+
+
+// // showketails(55,'ahmed',true);
+// // showketails(55,'AHEMD',true);
+// // showketails( true,55,'ahmed');
+// // // if (name == arrry[1] && age ==arrry[0] && stat == arrry[1] ){
+// // //   document.write( `  :  haalo ${name}`);
+// // // document.write( `  :  your is age ${age}`);
+// // // document.write( `  :  you good ${stat}`);
+
+// // // }
+// // // if (name == arrry[1] && age ==arrry[2] && stat == arrry[0] ){
+// // // document.write( `  :  haalo ${name}`);
+// // // document.write( `  :  your is age ${age}`);
+
+
+// // // document.write( `  :  you good ${stat}`);
+
+// // // }
+// // let ali = document.getElementById("show").onclick = function zezo (){
+
+// // console.log("iove mama and father and sister");
+
+
+// // }
+
+
+
+
+
+
+
+// // //  داله تقوم بعمل معين عند وقت معين 
+// // // setTimeout (function(){
+// // // console.log('zezo yassin th goog');
+
+
+// // // },5000 )
+
+// // document.getElementById("show").onclick = function (){
+
+// // for(let i= 0; i< 10; i++){document.write(i)};
+// // }
+
+
+// // document.write()
+// // function print (){
+
+// // }
+
+
+// // function ahem (){
+// //   var aa =1;
+// //   let b =2;
+
+  
+// //   console.log(b);
+// // }
+
+
+
+// // ahem(); 
+
+
+// //ملاحضه مهمه انه عند تعريف متغبر من نوع var عايكةن عام حتئ داخل الداله بس ادا خليت نوع المتغبر LET عيكون خاثص الحلقه رقم 68 الزيرو
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // the map elemdnt send elment this pass lemnt 
+// //  let addself =[1,3,4,6,7,8];
+// //  let ahlii = addself.map (function(elemdnt, index , arry){
+  
+  
+
+//  function alshoy(alem){
+// return alem /2  ;
+
+
+//  }
+
+// let suabibg = "ahmedyaddin";
+//  let fofo =  suabibg.split().map( function(lee){
+
+// return lee === lee.toUpperCase() ? lee.toLowerCase() : lee.toUpperCase();
+//  }).join();
+//  console.log(fofo);  
+
+//  let zyad = [1,3,-4,5,-7,8];
+//  let wafa = zyad.map( function( lel){
+// return -lel;
+
+
+//  })
+//   console.log(wafa); 
+//  let assm = " rii456ljf";
+ 
+// /////////////////////////
+//  let rash= assm.split("").map(function(el){
+
+// return isNaN(parseInt(el)) ? el : "";
+
+
+
+//   }).join("");
+//   console.log(rash)
+
+//   console.log("ahmed yaddin")
+
+// let  dala = ["ahmed","kasm", "asma"];
+ 
+// let kasm  = dala.filter(function(ff){
+// // this function return word to char sendr
+//   return  ff.startsWith("a");
+// })
+// console.log(kasm);
+// // this function examine number sba and fardy
+// let numberss =[ 1,3,4,6,8,9,9,0];
+// let sosos = numberss.filter(function(ss){
+
+// return ss% 2 === 0 ? true : false
+// })
+// console.log(sosos);
+
+// // داله ترجع الكلمنات اللي حروفها اربعه تو اكبر
+
+// let dall = "i dont love dala i live the rash yaddin";
+// let asm =dall.split(" ").filter(function(eee){
+
+//   return eee.length < 5 ;
+// }).join(" ");
+// console.log(asm)
+// // داله راديوس ترجع قيمه واحده
+
+// let fun = [2,3,4,5,6,7,8,9];
+// let fa = fun.reduce(function(aa , carnt , index , arry){
+// return  carnt % 2 === 0 ? carnt  : aa;
+
+
+// })
+// console.log(fa);
+// //  اداق قمنا با ارسال قيمه ثاينيه لداهلراديوس  عايوقع الكرنت ممن اول موقع 
+
+
+//   // عند الضغط علئ العنصر يطبع في الكنسوب
+// let fah =  document.querySelectorAll(" ul li");
+
+// // fah.forEach(function (el){
+// //  el.onclick = function (){
+
+// //   fah.forEach (function(e){
+// //  // e.style.display = "none"
+
+// //   });
+ 
+// //  };
+
+// // });}
+
+
+
+// let sosoyasn =["yasmanaki", "koaks ", "soo","ahmdd aisl salysdf "];
+// let l = sosoyasn.reduce(function(arm,cont,index,arry){
+
+//   return arm.length > cont.length? arm :cont;
+// })
+// console.log(l);
+// let mystin = "1,2,3,4,5,6,7,ee,z,e,r,o,w,ap,a,-,schole"
+//  let finf = mystin.replace(", / - _").split("").map(function(ele ){
+//  isNaN(parseInt(ele)) ? ele : " "
+
+
+//  }).join("");
+//  console.log(finf);
+  
+ 
+// let user  ={
+
+// thnam : " ahmef ",
+// age :" 14",
+// kyaty : " abb",
+// adrss :{
+
+//   dya: "abe ", 
+//   dy:" sna",
+//   hos:{
+//   one : " ragy",
+//   tuo : " abdorhman",
+
+
+//   }
+// },
+
+// sayel : function(){
+
+//   console.log("ahemd yasin")
+// },
+// father:{ 
+
+// ahmd:["ahemd","rach","abrhym"]
+// ,
+// check : function(){
+// if(age ===14){console.loge("THIS GOOD")}
+
+// }
+// }}
+
+// console.log(user.check)
+// console.log(user.father.ahmd[1])
+
+
+// console.log(user.adrss.hos.one);
+
+// let us = {
+
+
+
+//   thnam: " salh"
+// }
+
+// thnam = " ali";
+// thnam =" AHMED ";
+// console.log(user.thnam);
+// console.log(user.age);
+// // طريقه لاستدعا الوبجت 
+// console.log(user["age"]);
+
+
+//  let ras= {
+//   age : 20
+//  };
+
+// //  الاضافه الئ داخل الكلاس
+ 
+// ras.contry = " abbe and snaa and aldnoah";
+//  console.log(ras.age)
+// // انشا كلاس بطريقه ثايه 
+//  let usrss = new Object();
+
+//  thisss =" kasm";
+// vars =100;
+// console.log(this)
+// console.log(window. vars)
+
+// `console.log(user);
+
+// let obj = Object.create({
+//   sss:20,
+//   lt: function (){
+// return this.sss * 2 ;
+
+//   }
+// });
+// // استجدامك ال this
+// console.log(obj.lt());
+
+// console.log(obj.sss);
+// //  عمليه وراثه
+// let o = Object.create(obj)
+// obj.sss =30;
+// console.log(o.lt());
+
+
+
+// let mydid = document.getElementById("foris");
+
+// console.log(mydid);
+// // استدعا البارجراف  من داخل العنصر
+// let elmtnts = document.getElementsByTagName("p");
+// console.log(elmtnts[1])
+
+// // عشان نستدعي مكون الا html
+
+// elmtnts[1].innerHTML = "testing";
+// console.log(elmtnts[3]);
+// // ارسلها العناصر وهيه تجيبها 
+// let myqury = document.querySelectorAll(" .spain")
+
+// // يجيب العنوان تبع الصفحع 
+// console.log(document.title);
+
+// // وصلنا لكل الفورمات  بعدا اول فورم بعدا قيمه الفرم
+// // console.log(document.forms[0].one.value);
+// let lets  = document.querySelector("span")
+// // تجيب المحتوئ النصي
+// console.log(lets.textContent);
+
+// let mylinke = document.querySelector(".linke");
+ 
+//   // هاده الخاصيه عشان نوصل للخصائض
+// console.log(mylinke.getAttribute("class"))
+
+// // هاده الخاصيه نرسل لها اسم الخاصيه اللي نشتي نعدل عليها ونرسل لها القيمه الجديده 
+// mylinke.setAttribute("href", " https://twitter.com");
+
+
+
+// console.log(document.getElementsByTagName("p")[0].attributes);
+// // يجيب كل خصائص العنصر المرسل
+// let pargaf = document.getElementsByTagName("p")[0];
+// console.log(pargaf.hasAttribute("data-src"));
+
+
+// // هاده تفحص ادا كان موحوداو لا
+// if(pargaf.hasAttribute("data-src")){
+
+
+// console.log("not funed");
+// }
+// else console.log( pargaf.getAttribute("data-src"));
+// // ازاله الخاصيه 
+// pargaf.removeAttribute("class");
+// انشا العنصر
+// let att = document.createElement("div");
+//  att.className =" kasm";
+//  console.log(att);
+// //  انشا الخاصيه
+//  let at = document.createAttribute("id");
+// // اضافه الخاصيه الئ العنصر
+//  att.setAttributeNode(at);
+// // اضافه اسم الخاصيه 
+//  att.setAttribute("id", "osamwalyed");
+ 
+// //   الدي في الكنسل ركز  ليس الئ الصفحه اضافه النص الئ داخل العنصر حقما
+//  let text = document.createTextNode(" ahmeyadin");
+//  att.appendChild(text);
+// //  the append to  body
+
+ 
+//  document.body.appendChild(att);  
+ 
+// let te =document.createTextNode(" ahemd");
+
+//  let atss  = document.createElement("div");
+//  att.appendChild(atss);
+
+//  atss.appendChild(te);
+//  atss.appendChild(te);
+//  atss.className =  " zyad";
+//  let sooo= document.createAttribute("color");
+ 
+//  atss.setAttributeNode(sooo);
+//  for (let i=0 ; i< 99;i++){
+//   let aaa= document.createElement("div"); 
+//   let sd = document.querySelector("div")[i];
+ 
+  
+//   att.appendChild(aaa);
+//  }
+
+//  document.links[0].onclick= function(en){
+
+// console.log(en);
+// // الغا عمل الحدث 
+// en.preventDefault();
+
+//  }
+
+// let sub = document.querySelector("[name ='alli']")
+
+// console.log(sub)
+
+// document.forms[0].onsubmit = function (e){
+// let usr =false;
+// let agevalid = false;
+// console.log(sub.value)
+// console.log(sub.value.length);
+
+
+// if (sub.value !== 0 && sub.value.length <= 10 ){
+//     usr = true;
+
+// }
+
+// if ( usr === false || agevalid ===false){ e.preventDefault()}
+// }
+
+
+
+
+
+
+// جلب التكسنن عن طريق اسمه ثم فحص اداكان غير فاضي 
+
+// let zeozz = document.querySelector("[name='alli']");
+ 
+
+// let al =document.forms[0].onsubmit = function (e) {
+
+//     let a =false;
+//     let f = false;
+// if( zeozz.value !== 0 && zeozz.value.length <=10)
+// {
+//     a = true;
+// }
+// else{
+//     console.log("he dont avrge")
+
+// }
+// if ( a === false || f ===false){
+//     console.log("this goode ")
+//     e.preventDefault();
+// }
+
+// }
+
+//////////////////////////////////////////////////////////
+
+
+
+
+
+// let aloshy = document.getElementById("zyad-yasisn");
+
+// console.log( aloshy.classList);
+// //   نفحص ادا كان الدف او اي عنصر معه كلاس  او لا 
+// console.log(aloshy.classList.contains("zyad"));
+// // ظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظ
+
+// // داله نرسل لها الموقع وهيه ترجع ادا كان فيه كلاس 
+//  console.log(aloshy.classList.item("0"))
+
+//  aloshy.onclick = function (t){
+// // عند النقر علئ الزر ضيف كلاس وحدفه 
+// aloshy.classList.add("yasinali");
+// aloshy.classList.remove("yasinali");
+
+//  };
+//  console.log(aloshy.classList.item("1"));
+
+// let ko = document.getElementById("zeo");
+//  ko.onclick =function (){
+// //  داله ترسل لها  اسم الكلاس ادا كان موجود تحدفه زادا مش موجود تضيه 
+//     ko.classList.toggle("ko");
+//  }
+
+// // جلب عناصر ال css
+//  ko.style.color = "red";
+//  ko.style.background = " red"
+// ko.style.setProperty("font-size","40px");
+//  ko.style.cssText = "background:red; color: green; opacity:";
+//  ////////////////////////////////////////////////////////////\
+// //  حدف عناصر ال CSS
+//  ko.style.removeProperty("color")
+
+
+// //  اضافه النص قبل العصر ko
+//  ko.before("hali");
+//  let cratp = document.createElement("p");
+//  ko.before(cratp);
+// //  اضافه الئ النهايه 
+// ko.append(cratp);
+//  //////////////////////
+
+// //  ازاله العنصر من الجدور
+
+
+// //  ko.remove();
+// //////////////////////////////////////////////////////////////////////////////////////////////
+ 
+// let yy = document.querySelector(".ko");
+// // وحدفه هنا قلنا له يجب العصر  اللي بهد العنصر yy
+// yy.nextElementSibling.remove();
+// //////////////////////////////////////////////////////////////////////////////////////////
+// // اخد العنصر بدون التئثير عليه  نشل العنثرمع اللي داله 
+// let myp = document.querySelector("p").cloneNode(true);
+  
+//      ko.appendChild(myp);
+//      console.log(myp);
+
+//     //  //////////////////////////////////////////////////////////////////
+
+//     myp = onclick = one;
+//  function one() {
+
+//     console.log("يبشيسبمنشيستبميس ")
+//  }
+
+
+// //  ///////////////////////////////////////?????????
+
+// let rash  = document.querySelector("h1");
+
+// myp = onclick = one;
+// function one() {
+
+//    console.log("abrham habshy ")
+// }
+// // عند الضغط علئ العنصر يطبع لي اسامه 
+// rash.addEventListener("click", function(){
+
+// console.log("orm")
+
+// })
+// rash.addEventListener("click",one)
+// // ????????????????????????????????????????????
+
+// function to(){
+
+
+//     console.log("oneoyaddin");
+// }
+
+// rash.addEventListener("click",to);
+
+////////////////////////////////
+// عند الضغط علئ العنصر انشئ  واجد جديد
+// let rash  = document.querySelector("h1");
+
+// rash.onclick  = function(){
+
+//     let newp = rash.cloneNode(true);
+//     document.body.appendChild(newp)
+// }
+
+
+let sh = document.createElement("div");
+let s = document.createTextNode(" ahms yadins kasm ");
+document.body.appendChild(sh);
+window.document.title = "ahmd yaddin";
+
+//  داله القت تنتظر ثلاث ثواني وبعدا ترجع تشتغل
+// setTimeout(() => {
+//     console.log(`yadin ahemd yadidn`)
+// }, 3000);
+
+// function ahli (){
+
+// console.log(" abo flah")
+// }
+// setTimeout(ahli,  5000 )
+ 
+
+// داله الوقت نقوم با ارسال البارميتر ليس الئ داخل الاقواس بل الئ بعد الوقت
+function lter (uer){
+
+    console.log(` hama ${uer}`);
+
+}
+//  Timeout(lter, 3000,"ahme")
+////////////////////////////////////////////////////////////////////////////////////////////
+let t = document.querySelector("button");
+//  هادا حدث عند الصغط عليه يوقف داله الوقت
+
+t.onclick = function ()
+{ 
+    clearTimeout(tot);
+
+
+}
+
+let but2 =document.getElementById("but");
+but2.onclick = function (){
+    let tot =  setTimeout(lter ,3000);
+
+
+}
+
+ function ali(){
+
+    console.log("halo hmadi");
+ }
+
+ let ff = setTimeout(ali, 3000 );
+ console.log(`id:  ` + ff );
+ console.log(ff);
+// داله تطبع الوقت كل دقيقه 
+ function shotiem(){
+let aa = new Date();
+
+let TIEM =aa.toLocaleTimeString();
+
+
+console.log(TIEM);
+
+setTimeout ( shotiem, 3000);
+ }
+// داله تطبع الوقت
+ let f= new Date()
+ console.log(f );
+ console.log( f.toLocaleTimeString());
+////////////
+
+// this setInterval print the massge evry ment 
+ function abo(){console.log(`hlo ali abn yassin`)};
+//  setInterval(abo, 1000);
+ console.log(`hmody yaddin`);
+ /////////////////////////////////////////////////
+//  واداوقع قيمته واحد توقفه  داله تنقص المحمو الي داخل الدف كل ملرخ بواحد
+    let p = document.getElementById("ahm");
+
+    function aho(){
+p.innerHTML -= 1;
+if (p.innerHTML == 0){
+
+    clearInterval(fa)
+}
+    }
+
+    let fa = setInterval(aho, 1000);
+    // ظظظظظ ظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظ
+
+// console.log(location.href)
+
+// location.href = "https://googel.com";
+
+let wid = window.open("https://googel.com", "", "width=500 , height=100")
+kalid and kahsa ghjkkl;'cvbnm,./lkjhfdefd
+
+
+
+
+
+'
